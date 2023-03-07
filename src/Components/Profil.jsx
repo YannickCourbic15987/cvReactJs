465979846a873f8bf4016aa6eca41937a1637311
YannickCourbic15987 <a.yacine2020@laposte.net>
2023-03-07 08:53:30 +0100

    first deploy

--- src/Components/Profil.jsx
+++ src/Components/Profil.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 function Profil() {
   return (
-    <div className='profil mb5' style={{zIndex: "-1", marginTop:"-75px"}}>
+    <div className='profil mb5' style={{zIndex: "-1"}}>
         <h2 className='h2'>Profil</h2>
         <p>Diplômé d'un titre à l'AFCI de Villeneuve-d'Ascq et passionné de développement web, je souhaite poser ma candidature en tant que développeur web. Rigoureux, opportun et sérieux, j'ai l'intention d'utiliser mes compétences pour appuyer un projet de création d'une application Web fonctionnelle et fiable.</p>
     </div>
   )
 }
-
 export default Profil
